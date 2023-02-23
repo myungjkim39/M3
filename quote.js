8fdc1dfba77375bc4f5b10f68ff00da1a7048daf
MJ Kim <mjk@missionreadyhq.com>
2023-02-23 16:27:44 +1300

    update

--- quote.js
+++ quote.js
@@ -1,26 +1,19 @@
-const express = require("express");
-const env = require("dotenv");
-env.config();
-const server = express();
+function quote(carValue, riskRating) {
+  const yearlyPremium = (carValue * riskRating) / 100;
+  const monthlyPremium = yearlyPremium / 12;
+
+  if (carValue === null) {
+    return "Missing car value, please enter the numbers";
+  } else if (carValue < 0 || riskRating < 0) {
+    return "The value cannot be a negative number";
+  } else if (riskRating === 0 || riskRating >= 5) {
+    return "Risk rating needs to be between 1 to 5";
+  } else
+    return {
+      "yearly_premium": yearlyPremium,
+      "monthly_premium": monthlyPremium,
+    };
+}
 
 
-// PORT
-const PORT = process.env.PORT || 3002;
-server.listen(PORT, () => {
-  console.log("listening to port, PORT");
-});
-
-
-// Endpoints 
-server.get("/quote", (req, res) => {
-  res.send("this is quote API!");
-});
-
-// function quote(carValue, driverRating) {
-//   const yearlyPremium = (carValue * driverRating) / 100;
-//   const monthlyPremium = yearlyPremium / 12;
-
-//   return [yearlyPremium, monthlyPremium];
-// }
-
-// module.exports = quote;
+module.exports = quote;
--- quotefunction.test.js
+++ quotefunction.test.js
@@ -1,62 +1,71 @@
-// const quote = require("./quote");
-// const { expect } = require("chai");
-// const assert = require("assert");
+const quote = require("./quote");
+const { expect } = require("chai");
 
-// const testCases = [
-//   //positive cases
-//   {
-//     scenario: "Both inputs are numbers",
-//     input: [4800, 2],
-//     expected: [96, 8],
-//   },
-//   {
-//     scenario: "Car value as a string format of number",
-//     input: ["4800", 2],
-//     expected: [96, 8],
-//   },
+const testCases = [
+  {
+    scenario: "Both inputs are numbers",
+    carValue: 4800,
+    riskRating: 2,
+    expected: {
+      yearly_premium: 96,
+      monthly_premium: 8,
+    },
+  },
+  {
+    scenario: "Car value as a string format of number",
+    carValue: "4800",
+    riskRating: 2,
+    expected: {
+      yearly_premium: 96,
+      monthly_premium: 8,
+    },
+  },
+  {
+    scenario: "Risk rating as a string format of number",
+    carValue: 4800,
+    riskRating: "2",
+    expected: {
+      yearly_premium: 96,
+      monthly_premium: 8,
+    },
+  },
+  {
+    scenario: "Risk rating as 0",
+    carValue: 4800,
+    riskRating: 0,
+    expected: "Risk rating needs to be between 1 to 5",
+  },
+  {
+    scenario: "Risk rating above 5",
+    carValue: 4800,
+    riskRating: 6,
+    expected: "Risk rating needs to be between 1 to 5",
+  },
+  {
+    scenario: "Negative risk rating number",
+    carValue: 4800,
+    riskRating: -1,
+    expected: "The value cannot be a negative number",
+  },
+  {
+    scenario: "Negative Car value number",
+    carValue: -4800,
+    riskRating: 2,
+    expected: "The value cannot be a negative number",
+  },
+  {
+    scenario: "Missing car value",
+    carValue: null,
+    riskRating: 5,
+    expected: "Missing car value, please enter the numbers",
+  },
+];
 
-//   {
-//     scenario: "Risk rating as a string format of number",
-//     input: [4800, "2"],
-//     expected: [96, 8],
-//   },
-//   {
-//     scenario: "Risk rating as 0",
-//     input: [4800, 0],
-//     expected: ["Risk Rating needs to be at least 1"],
-//   },
-//   {
-//     scenario: "Risk rating value higher than 5",
-//     input: [4800, 6],
-//     expected: ["Risk Rating value needs to be below 6"],
-//   },
-//   {
-//     scenario: "Risk rating value lower than 1",
-//     input: [4800, -1],
-//     expected: ["Risk Rating value needs to be at least 1"],
-//   },
-//   {
-//     scenario: "Negative Car value number",
-//     input: [-4800, 1],
-//     expected: ["The Car value cannot be a negative number"],
-//   },
-//   {
-//     scenario: "Missing value",
-//     input: [null, 1],
-//     expected: ["Missing car value"],
-//   },
-// ];
-
-// describe("quote tests", () => {
-//   testCases.map((tc) => {
-//     it(tc.scenario, () => {
-//       const [yearlyPremium, monthlyPremium] = quote(tc.input[0], tc.input[1]);
-//       expect(yearlyPremium).to.equal(tc.expected[0]);
-//       expect(monthlyPremium).to.equal(tc.expected[1]);
-
-//       // check if expected value is negative
-//       assert(tc.expected[0] >= 0, "Expected yearlyPremium to be non-negative");
-//       assert(tc.expected[1] >= 0, "Expected monthlyPremium to be non-negative");
-//     });
-//   });
-// });
+describe("quote tests", () => {
+  testCases.map((tc) => {
+    it(tc.scenario, () => {
+      const actual = quote(tc.carValue, tc.riskRating);
+      expect(actual).to.deep.equal(tc.expected);
+    });
+  });
+});
